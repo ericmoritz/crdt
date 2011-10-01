5a3776a31eb79431e8eb68475a1d3beffc7c667a
Eric Moritz <eric@themoritzfamily.com>
2011-10-01 04:16:44 -0400

    Added my attempt at a add/remove set using counters.  I'm pretty sure the way I'm using the A/R counters is a semilattice.

--- crdt/sets.py
+++ crdt/sets.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from base import StateCRDT
+from base import StateCRDT, random_client_id
 from copy import deepcopy
 from collections import MutableSet
-
+from counters import GCounter
+import uuid
 
 class GSet(StateCRDT, MutableSet):
     def __init__(self):
@@ -181,4 +182,132 @@ def test_towpset():
 
     ABB2 = TwoPSet.merge(AB, B2)
     assert ABB2.value == {"eric", "mark", "glenn"}, ABB2.value
+
+
+class EMSet(StateCRDT, MutableSet):
+    def __init__(self, client_id=None):
+        self.client_id = client_id or random_client_id()
+        self._payload = {}
+
+    @classmethod
+    def _get_counters(cls, emset, element):
+        if element in emset.payload:
+            a, r = emset.payload[element]
+            return (GCounter.from_payload(a, client_id=emset.client_id),
+                    GCounter.from_payload(r, client_id=emset.client_id))
+        else:
+            return (GCounter(client_id=emset.client_id),
+                    GCounter(client_id=emset.client_id))
+
+    def add(self, element):
+        A, R = self._get_counters(self, element)
+
+        # if the A counter is less than or equal to the R counter,
+        # increment the R counter and replace the A payload making A > R
+        if A <= R:
+            newA = R.clone()
+            newA.increment()
+
+            self.payload[element] = (newA.payload, R.payload)
+
+        # We don't have to do anything if the A_counter is already
+        # creater than the R counter
+
+    def discard(self, element):
+        A, R = self._get_counters(self, element)
+        
+        # If the R value is less than the A value
+        # take the A value, increment it and replace R
+        if R < A:
+            newR = A.clone()
+            newR.increment()
+            self.payload[element] = (A.payload, newR.payload)
+
+    @property
+    def value(self):
+        def gen():
+            for element in self.payload:
+                A, R = self._get_counters(self, element)
+                if A > R:
+                    yield element
+
+        return set(gen())
+
+    def get_payload(self):
+        return self._payload
+
+    def set_payload(self, payload):
+        self._payload = payload
+
+    payload = property(get_payload, set_payload)
+
+    @classmethod
+    def merge(cls, X, Y):
+        payload = {}
+
+        elements = set(X.payload) | set(Y.payload)
+
+        for element in elements:
+            Ax, Rx = cls._get_counters(X, element)
+            Ay, Ry = cls._get_counters(Y, element)
+
+            newA = GCounter.merge(Ax, Ay)
+            newR = GCounter.merge(Rx, Ry)
+
+            payload[element] = (newA.payload, newR.payload)
+
+        return cls.from_payload(payload)
+
+    def compare(self, other):
+        pass
+
+    def __contains__(self, element):
+        A, R = self._get_counters(self, element)
+        return A > R
+
+    def __iter__(self):
+        return self.value.__iter__()
+    
+    def __len__(self):
+        return self.value.__len__()
+
+def test_emset():
+    """
+        {},{}
+      /       \
+  A{eric},{}   B{glenn},{}          +eric +glenn
+    |             \ 
+ A{eric mark},{} B{glenn tom},{}   +mark +tom
+    |             /   \ 
+    |           /      \
+     \         /        \
+      \       /     B2{glenn tom},{tom}   -tom
+        \   /              \ 
+   AB{eric mark tom glenn}  \   <<merge>>
+            \               /
+             \             / 
+    ABB2{eric mark tom glenn},{tom}   <<merge>>
+
+    """      
+    A = EMSet()
+    B = EMSet()
+    
+    A.add("eric")
+    B.add("glenn")
+    
+    A.add("mark")
+    B.add("tom")
+
+    AB = EMSet.merge(A, B)
+
+    B2 = B.clone()
+    B2.remove("tom")
+
+    ABB2 = EMSet.merge(AB, B2)
+    assert ABB2.value == {"eric", "mark", "glenn"}, ABB2.value
+
+    B2.add("tom")
+    
+    ABB2 = EMSet.merge(ABB2, B2)
+    assert ABB2.value == {"eric", "mark", "glenn", "tom"}, ABB2.value
 
